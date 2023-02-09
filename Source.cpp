eaf0d52a032f84e46c41e01ee6e0507cf5c1a72f
jamesl3483 <jlukas6249@gmail.com>
2023-02-09 15:08:02 -0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -9,12 +9,6 @@
 using namespace std;
 
 
-//The main thing is how to solve the puzzle. In the file that the program is saved,
-//perhaps there is a board first, and then the answers afterwards, and all the program
-//does is find those words. It's doable.
-
-//Also, looking at the example, there is no overlap between the correct answers.
-
 const char alphabets[26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
 class WordSearchBoard
